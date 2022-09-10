eaa007f5a14d0f9e3990ad44a493ae8fab1803b4
Daniel Reis <daniel.reis@rain.us>
2022-09-10 12:33:15 -0300

    feat: call read and save from main package

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/danieeelfr/my-large-json-file-reader/internal/config"
+	"github.com/danieeelfr/my-large-json-file-reader/internal/repository"
+	"github.com/danieeelfr/my-large-json-file-reader/internal/service"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	_ = config.NewConfig()
+	cfg := config.NewConfig()
+
+	repo := repository.NewRepository(cfg)
+	srv := service.NewPortService(cfg)
+
+	ports, err := srv.ReadAndDecode()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := repo.Set(ports.Records); err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO
+	// result, err := repo.Get("", 100)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	fmt.Println("finished without errors!")
 }
 
 func init() {
